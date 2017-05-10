2d95522c5c811340a98c93725cf80c866c26b07c
s.thareja <s.thareja@accenture.com>
2017-05-10 11:55:47 +1000

    initial commit for app

---
+++ app.js
@@ -0,0 +1,12 @@
+app.post('/hello', function (req, res, next) {
+  var userName = req.body.user_name;
+  var botPayload = {
+    text : 'Hello ' + userName + ', welcome to Saurabh Thareja Slack channel! I\'ll be your guide.'
+  };
+  // Loop otherwise..
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+});
